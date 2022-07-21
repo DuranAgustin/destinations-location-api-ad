52afeee8785a2a04fc9fff418544d9db2c779f7e
Agustin <agustinduranse@gmail.com>
2022-07-21 07:20:07 -0700

    first

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const server = express(); // This server is deaf
+
+//server.listen(8000); // Told the server to listen on port 3000
+const PORT = process.env.PORT || 8000;
+//just to conole to make sure it is listening
+server.listen(PORT, () => {
+  console.log(`Server listening on port:${PORT}`);
+});
+
+const destinationsDB = {
+  123456: {
+    destination: "Eiffel Tower",
+    location: "Paris",
+    photo:
+      "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+  234567: {
+    destination: "Big Ben",
+    location: "London",
+    photo:
+      "https://images.unsplash.com/photo-1529655683826-aba9b3e77383?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+  },
+};
